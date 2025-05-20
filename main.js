a8135b58ba3e44536c10a36c1ff1348b1a99df7f
Janaina da Silva Rodrigues <145395931+dasilvajanaine@users.noreply.github.com>
2025-05-20 14:27:22 -0300

    kk

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const  botoes  =  documento . querySelectorAll ( ". botao" );
+const botoes = document.querySelectorAll(".botao");
 
-para ( deixe  i  =  0 ; i  <  botoes . comprimento ; i ++ ) {
-    botoes [ i ]. onclick  =  função () {
-        botoes[i].classList.add("ativo");
-    };
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+  };
 }
