de3a6aeea307879bc1440c6c6e5b26fb5c9fe535
Janaina da Silva Rodrigues <145395931+dasilvajanaine@users.noreply.github.com>
2025-06-03 14:51:00 -0300

    gg

--- main.js
+++ main.js
@@ -15,7 +15,34 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(" .contador");
-const tempoObjetivo1 = new date("2025-12-19T00:00:00");
-let tempoatual = new date();
+<<<<<<< HEAD
+const tempoobjetivo1 = new date("2025-12-19T00:00:00");
 
+
+contadores(0); textcontent = calculatempo(tempoobjetivo); 
+
+function calculatempo(tempoobjetivo1);
+=======
+const tempoObjetivo1 = new date("2025-12-19T00:00:00");
+>>>>>>> 63a9b09c2f6f6d97e8e7b41a347dfb75186c029d
+let tempoatual = new date();
+let tempofinal =tempoobjetivo-tempoatual;
+let segundos = math.floor( tempofinal /1000); 
+let minutos = math.floor( segundos /60);
+let horas = math.floor(minutos /60);
+let dias = math.floor(horas /60);
+
+<<<<<<< HEAD
+=======
 contadores(0); textcontent = tempoObjetivo1 - tempoatual;
+>>>>>>> 63a9b09c2f6f6d97e8e7b41a347dfb75186c029d
+
+seundos *= 60;
+minutos *= 60;
+horas *= 24;
+
+return(dias + "dias" + horas +" horas " + minutos  +" minutos"  +segumdos + "segundos");
+
+
+    
+
